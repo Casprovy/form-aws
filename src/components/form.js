68a2f2d294840134364431d62059a72535e91ff4
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-05-20 21:33:28 +0200

    working on buttons logic

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
 		this.state = {
 			message: '',
 			email: '',
 		};
 	}
 
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	clearForm = () => {
+		this.setState({
+			message: '',
+			email: '',
+		});
+	};
+
 	render() {
 		return (
 			<div>
 				<h2>it works!</h2>
+				<form>
+					<input
+						type='text'
+						value={this.state.message}
+						name='message'
+						placeholder='your message'
+						onChange={this.handleChange}
+					/>
+					<br />
+					<input
+						type='email'
+						value={this.state.email}
+						name='email'
+						placeholder='your email'
+						onChange={this.handleChange}
+					/>
+				</form>
+				<h2>
+					{this.state.message} {this.state.email}
+				</h2>
+				<button onClick={this.clearForm}></button>
 			</div>
 		);
 	}
